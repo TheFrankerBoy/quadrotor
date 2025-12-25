4ff083614556ac69e63340cb2d2ef6010603e0b9
Fran <franfuentes2004@gmail.com>
2025-12-25 18:05:59 +0100

    Inicializador

---
+++ ini.bash
@@ -0,0 +1,8 @@
+cd /opt/ros/iron
+source setup.sh
+cd /ros2_ws
+colcon build --packages-select sjtu_drone_control --symlink-install
+source install/setup.bash
+ros2 run sjtu_drone_control trajectory_follower --ros-args -r __ns:=/drone
+
+

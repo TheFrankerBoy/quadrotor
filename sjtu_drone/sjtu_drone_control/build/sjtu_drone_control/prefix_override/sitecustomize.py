bd86ae871f0f0dad781f54e768cf7e1d5501a0fa
antlagsol10 <antlagsol@gmail.com>
2026-01-02 21:25:14 +0100

    Creacion de la branch para PX4 con su estructura base definida

--- sjtu_drone/sjtu_drone_control/build/sjtu_drone_control/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/ros2_ws/src/sjtu_drone/sjtu_drone_control/install/sjtu_drone_control'
--- sjtu_drone/sjtu_drone_control/build/sjtu_drone_control/share/sjtu_drone_control/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/ros2_ws/src/sjtu_drone/sjtu_drone_control/build/sjtu_drone_control"

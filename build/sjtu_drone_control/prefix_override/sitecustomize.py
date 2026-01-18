d50e1edab197881d176a231e0ab549a11cf82768
FranFuentes <70015682+TheFrankerBoy@users.noreply.github.com>
2026-01-18 19:06:59 +0100

    Delete build directory

--- build/sjtu_drone_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/franfuentes/ros2_sjtu_ws/src/install/sjtu_drone_control'
--- build/sjtu_drone_control/share/sjtu_drone_control/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/franfuentes/ros2_sjtu_ws/src/build/sjtu_drone_control"
